f3f9bb29f6e3f6d5d50825669043f4de8c394713
sundaee1147 <Ogbodudavid10@gmail.com>
2023-07-22 12:40:15 +0100

    Express Checkpoint

---
+++ routes/route.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/services",(req,res)=>{
+    res.render("OurServices");
+});
+
+
+router.get("/contact",(req,res)=>{
+    res.render("ContactUs");
+});
+
+
+module.exports = router;
